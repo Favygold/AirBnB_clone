0d517f877586e302ed3bf58e5ed4ee056dbf5d70
Bright <brightiniabasi@gmail.com>
2022-08-03 11:03:57 +0100

    Added Base class :construction:

--- models/base_model.py
+++ models/base_model.py
@@ -18,13 +18,13 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-        tformat = "%Y-%m-%dT%H:%M:%S.%f"  # datetime format
+        dtformat = "%Y-%m-%dT%H:%M:%S.%f"  # datetime format
 
         if kwargs:
             for key, value in kwargs.items():
                 if key != '__class__':
                     if key == 'created_at' or key == 'updated_at':
-                        self.__dict__[key] = datetime.strptime(value, tformat)
+                        self.__dict__[key] = datetime.strptime(value, dtformat)
                     else:
                         self.__dict__[key] = value
         else:
